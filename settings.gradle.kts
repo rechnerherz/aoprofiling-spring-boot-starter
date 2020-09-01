d07f8f56a10d684c36fdd535e05cfa42e1d056f2
Dario Seidl <darioseidl@fastmail.fm>
2020-09-01 17:07:35 +0200

    Change root name

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,10 @@
 // Custom Spring Boot Starter
 // https://docs.spring.io/spring-boot/docs/current/reference/html/spring-boot-features.html#boot-features-custom-starter
 
-rootProject.name = "aoprofiling-spring-boot-starter"
+rootProject.name = "aoprofiling-spring-boot-starter-root"
 
 include(
+    "aoprofiling",
     "aoprofiling-autoconfigure",
-    "aoprofiling-spring-boot-starter",
-    "aoprofiling"
+    "aoprofiling-spring-boot-starter"
 )
